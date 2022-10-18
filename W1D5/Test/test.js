79a654fde0c0437798eebc5ec28d0fd1c0e27dd4
Awais Iftikhar Xsatti <awaisiftikhar90@gmail.com>
2022-10-18 03:40:24 -0500

    W3D1
    Classess And constructor

--- W1D5/Test/test.js
+++ W1D5/Test/test.js
@@ -1,4 +1,25 @@
 
+
+// Quiz
+
+var b = 5;
+var c = hello(b);
+var a = 1000;
+var d = {a: 50, b: 90};
+console.log(c.apply(d));
+function hello(a) {
+    console.log(this);
+    a = a * 2;
+    console.log(bye())
+    function bye() {
+        console.log(this);
+        console.log(b);
+        console.log(this.b);
+        return a;
+    }
+    var b = 100;
+    return bye;
+}
     
     // let user ={ name:"Awais"};
     // let admin = user; // copy the reference 
